8bd8597d4d7052cbfa4e6a035bfdf7436a0c0cac
Juan Pablo OLIVADOTI <jp.olivadoti@gmail.com>
2016-12-25 23:29:49 +0100

    Changing view

--- assets/js/init.js
+++ assets/js/init.js
@@ -4,73 +4,59 @@ $(document).on("ready", function(){
 
   var list  = [];
   var items = [];
+  var data = "";
+  var i = 1;
+  $.ajax({
+    url: "http://pokeapi.co/api/v2/pokemon/",
+    success:  manageResults,
+    error: handleError
+  })
 
   function manageResults (response){
 
     var data = response.results;
-    var i = 1;
-
-    data.sort(function(a, b){
-      var nameA=a.name.toLowerCase(), nameB=b.name.toLowerCase()
-      if (nameA < nameB)
-          return -1
-      if (nameA > nameB)
-          return 1
-      return 0
-    })
 
     data.forEach(function(item){
       var name  = item.name.toUpperCase();
       var pkurl = item.url;
 
-      console.log(i);
-
-      list.push(name);
-      console.log(name);
-
       var poke = `
         <div class="col s12 m4">`+
           `<div class="carousel-cell">`+
-            `<a class="carousel-item" href="`+pkurl+`">`+
+            `<a class="carousel-item" href="#" data-src="`+pkurl+`">`+
               `<img class="center-block" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/`+i+`.png" alt="`+name+`">`+
               `<h4 class="center-align">`+name+`</h4>`+
             `</a>`+
           `</div>`+
       `</div>
         `
-
-      console.log(poke);
-
       $('#main-carousel').append(poke);
 
       i++
 
     })
-
-    for(i in data)
-    items.push([i,data[i]]);
-
-    // console.log(items);
-
-
   };
-
-  $.ajax({
-    url: "http://pokeapi.co/api/v2/pokemon/",
-    success:  manageResults
-  })
 
   $(document).ajaxComplete(function(){
 
     $('.main-carousel').flickity({
       // options
-      cellAlign: 'left',
+      cellAlign: 'center',
       contain: true,
       wrapAround: true
     });
 
-    // $('.carousel').carousel();
   });
+
+
+
+  function getPokeInfo(response){
+    console.log(response);
+  }
+
+  function handleError(err){
+    console.log(err);
+  }
 
 });
 
